2aee0a74c9e116f5ec97e64ef60ef0d47f628148
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-22 19:52:54 +0000

    Implement renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function(){
-  renderTweets(data);
+  // Ajax call for GET-ing all tweets
+  function loadTweets(){
+    $.ajax({
+      url: '/tweets',
+      method: "GET"
+    }).done(renderTweets);
+  };
 
+  // Render all tweets
+  loadTweets();
+
+  // Ajax call for POST-ing new tweet
   $("form[action='/tweets']").on('submit', function(event) {
     event.preventDefault();
 
@@ -13,11 +23,11 @@ $(document).ready(function(){
       url: '/tweets',
       method: 'POST',
       data: $(this).serialize()
-    }).done(function(){})
-      .fail(function(err){});
-
+    }).done(function(){
+      loadTweets();
+      $(".new-tweet").find("textarea[name='text']").val('');
+    });
   })
-
 });
 
 function createTweetElement(tweetData) {
@@ -56,51 +66,3 @@ function renderTweets(tweets) {
     $("#tweets-container").append($tweet);
   }
 }
-
-// Fake data taken from tweets.json
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
