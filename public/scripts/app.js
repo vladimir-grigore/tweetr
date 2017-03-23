938fb8561537dfb18d36d927eab2e6ae0b39a025
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-23 01:17:44 +0000

    Fix minor spacing issues

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(function(){
   // Ajax call for GET-ing all tweets
   function loadTweets(){
