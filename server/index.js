c25ce870c1ce4973faa23a53b8aa5d5be82d660a
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-26 17:17:30 +0000

    Change port number to process.env.PORT

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 // Basic express setup:
 require('dotenv').config();
 
-const PORT          = 8080;
+const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
